4157215881209b2b9712c03e4d139f25280630de
lokesh1904 <43114991+lokesh1904@users.noreply.github.com>
2018-10-07 18:35:28 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:5672');
+   define('DB_USERNAME', 'misil');
+   define('DB_PASSWORD', 'Misil490.');
+   define('DB_DATABASE', 'user_info');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
