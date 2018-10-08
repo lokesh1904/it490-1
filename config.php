99d1f39dd4b97f4f2f4cdd9ae97e8d781f4c6473
lokesh1904 <43114991+lokesh1904@users.noreply.github.com>
2018-10-07 21:04:44 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:5672');
-   define('DB_USERNAME', 'misil');
-   define('DB_PASSWORD', 'Misil490.');
-   define('DB_DATABASE', 'user_info');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
